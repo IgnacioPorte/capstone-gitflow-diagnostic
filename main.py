76b972b679409d5786e900d45076ad464bae91e5
Ignacio Porte <ignacio.porte@uc.cl>
2022-08-17 21:48:06 -0400

    feat: base

---
+++ main.py
@@ -0,0 +1,42 @@
+# Los top 10 tweets más retweeted.
+# Los top 10 usuarios en función de la cantidad de tweets que emitieron.
+# Los top 10 días donde hay más tweets.
+# # Los top 10 hashtags más usados.
+
+def top_10_retweeted():
+    """
+    Los top 10 tweets más retweeted.
+    """
+    pass
+
+
+def top_10_users():
+    """
+    Los top 10 usuarios en función de la cantidad de tweets que emitieron.
+    """
+    pass
+
+
+def top_10_days():
+    """
+    Los top 10 días donde hay más tweets.
+    """
+    pass
+
+
+def top_10_hashtags():
+    """
+    Los top 10 hashtags más usados.
+    """
+    pass
+
+
+def main():
+    top_10_retweeted()
+    top_10_users()
+    top_10_days()
+    top_10_hashtags()
+
+
+if __name__ == "__main__":
+    main()
