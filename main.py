12e9dca5e7f59ddd51cf92b6378a8d00b95640e0
Ignacio Porte <ignacio.porte@uc.cl>
2022-08-17 22:18:12 -0400

    feat: add top 10 retweeted

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+import json
 # Los top 10 tweets más retweeted.
 # Los top 10 usuarios en función de la cantidad de tweets que emitieron.
 # Los top 10 días donde hay más tweets.
-# # Los top 10 hashtags más usados.
+# Los top 10 hashtags más usados.
 
-def top_10_retweeted():
+def top_10_retweeted(data):
     """
     Los top 10 tweets más retweeted.
     """
-    pass
+    top_retweeted = []
+    for one_json in data:
+        top_retweeted.append((one_json['retweetCount'], one_json['content']))
+    # sort by retweet count
+    top_retweeted.sort(key=lambda x: x[0], reverse=True)
+    return top_retweeted[:10].map(lambda x: x[1])
 
 
 def top_10_users():
